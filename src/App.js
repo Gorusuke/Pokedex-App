b4dd849aff132228b65b1b5d9fb94eac20f72b82
Jose Andres <Gorusuke123@gmail.com>
2021-02-04 19:38:10 -0500

    Agregando un spinner y sus estilos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import {useState, useEffect} from 'react';
 import Axios from 'axios';
-import './App.css';
+// import './App.css';
 import Pokemon from './Components/Pages/Pokemon';
+import Loading from './Components/Pages/Loading';
 
 function App() {
 
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
 
-  const API = async () => {
-    const response = await Axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
-    setPokemons(response.data.results)
-  }
+  
 
   useEffect(() => {
+    const API = async () => {
+      const response = await Axios.get(initialUrl);
+      setNextUrl(response.data.next)
+      setPrevUrl(response.data.previous)
+      await loadingPokemons(response.data.results)
+      setLoading(false)
+    }
     API();
   }, [])
 
-  // console.info(pokemons)
+  const loadingPokemons = async (data) => {
+    let pokemones = await Promise.all(data.map(async pokemon => {
+      let getAllPokemon = await Axios.get(pokemon.url)
+      return getAllPokemon.data;
+    }))
+    setPokemonData(pokemones)
+  }
+
+  console.info(pokemonData)
 
   return (
     <div className="App">
-      {pokemons.map((pokemon, i) => 
-        <Pokemon
-          key={i}
-          url={pokemon.url}
-        />  
-      )}
+      {loading 
+        ? <Loading/> 
+        : pokemonData.map(pokemon => 
+          <Pokemon
+            key={pokemon.id}
+            id={pokemon.id}
+            weight={pokemon.weight} 
+            height={pokemon.height}
+            name ={pokemon.name}
+            abilities={pokemon.abilities} 
+            stats ={pokemon.stats}
+            types ={pokemon.types}
+            sprites={pokemon.sprites}
+          />  
+        )
+      }
+      
     </div>
   );
 }
--- src/Components/Pages/Pokemon.js
+++ src/Components/Pages/Pokemon.js
@@ -2,45 +2,25 @@ import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 
 
-const Pokemon = ({url}) => {
+const Pokemon = ({id, weight, height, name, abilities, stats, types, sprites}) => {
 
-  const [pokemones, setPokemones] = useState({})
-
-  const API = async () => {
-    const response = await Axios.get(url)
-    setPokemones(response.data)
-  }
+  // const [pokemones, setPokemones] = useState({});
   
-  useEffect(() => {
-    API();
-    // eslint-disable-next-line
-  }, [])
+  const pokemonTypes = () => {
+    const typesPokemon = types.map((type, index) => <p key={index}>{type.type.name}</p>)
+    return typesPokemon;
+  }
 
-  // console.info(pokemones)
-  const {id, weight, height, name, abilities, stats, types, sprites} = pokemones;
-  // const {other} = sprites
-  console.info(abilities)
-  console.info(stats)
-  console.info(types)
-  console.info(sprites)
-  // console.info(other)
-
-
+  
   return (
-    <>
+    <div>
       <div>
-        {/* <img src={pokemones.sprites.other['official-artwork'].front_default} alt={pokemones.name}/> */}
-        <h3>{name}</h3>
-        <p>{id}</p>
-        <p>{weight}</p>
-        <p>{height}</p>
+        <img src={sprites.other['official-artwork'].front_default} alt={name}/>
       </div>
-      {/* <div>
-        {types.map(type => 
-          <h5>{type.type.name}</h5>
-        )}
-      </div> */}
-    </>
+      <p>{id}</p>
+      <h3>{name}</h3>
+      <div>{pokemonTypes()}</div>
+    </div>
   )
 }
 
